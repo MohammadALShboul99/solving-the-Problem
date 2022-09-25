60336272f1ffac4ba130c2d3c0459c1235374921
MohammadALShboul99 <110593677+MohammadALShboul99@users.noreply.github.com>
2022-09-26 01:13:14 +0300

    update

--- lib/Component/MyWidget.dart
+++ lib/Component/MyWidget.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:problem/HomePage.dart';
 
 class MyWidget extends StatefulWidget {
-  MyWidget({required this.OnTap, required this.value});
+  MyWidget(
+      {required this.OnTap,
+      required this.value,
+      required this.myTextTitle,
+      required this.myTextSub});
 
   bool value = false;
+  String? myTextTitle;
+  String? myTextSub;
 
   void Function(bool)? OnTap;
   @override
@@ -16,6 +22,8 @@ class _MyWidgetState extends State<MyWidget> {
   Widget build(BuildContext context) {
     bool? myVal;
     return SwitchListTile(
+      title: Text("${widget.myTextTitle}"),
+      subtitle: Text("${widget.myTextSub}"),
       onChanged: widget.OnTap,
       value: widget.value,
     );
--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -15,28 +15,58 @@ class _HomeState extends State<Home> {
   bool myVal = false;
   bool myVal2 = false;
   bool myVal3 = false;
+  List<Color> myColor = [Colors.white, Colors.purpleAccent];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          MyWidget(
-            OnTap: (value) {
-              setState(() {
-                myVal3 = !myVal3;
-              });
-            },
-            value: myVal3,
-          ),
-          MyWidget(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(0, 226, 119, 245),
+      ),
+      body: Container(
+        decoration: BoxDecoration(gradient: LinearGradient(colors: myColor)),
+        child: Column(
+          children: [
+            MyWidget(
+              OnTap: (value) {
+                setState(() {
+                  myVal3 = !myVal3;
+                  if (myVal3 == true) {
+                    myColor = [Colors.pink, Colors.black];
+                  } else {
+                    myColor = [Colors.white, Colors.purpleAccent];
+                  }
+                });
+              },
+              value: myVal3,
+              myTextTitle: "Dark Mood",
+              myTextSub: "Dark Mood is wonderful",
+            ),
+            MyWidget(
               OnTap: (value) {
                 setState(() {
                   myVal2 = !myVal2;
+                  if (myVal2 == true) {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          content: Container(
+                            child: Column(
+                              children: [Text("This is The Information .")],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
                 });
               },
-              value: myVal2)
-        ],
+              value: myVal2,
+              myTextTitle: "Contact Us",
+              myTextSub: "Eg: PhoneNumber , Facebook , Our Location",
+            ),
+          ],
+        ),
       ),
     );
   }
